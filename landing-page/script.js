0f3a02d0d5c9b26868b352bc4ab84dfb79c446ed
gsramos1305 <139995325+gsramos1305@users.noreply.github.com>
2023-09-18 15:13:58 -0400

    .

--- landing-page/script.js
+++ landing-page/script.js
@@ -1,6 +1,6 @@
 function exibirConteudo() {
-    document.querySelector('.content').style.display = 'block'
-    document.querySelector('.content-2').style.display = 'block'
+    document.querySelector('.content').style.display = 'block';
+    document.querySelector('.content-2').style.display = 'block';
 }
 
 setTimeout(exibirConteudo, 3000)
